29c4f3071e9d54e4b0867e1a83646023fc0c49a0
leobardo ernesto sanchez == metalComido <leobardo.sanchez@uabc.edu.mx>
2021-05-13 13:12:38 -0700

    Python(CRUD) creating database with sqlite3 in DB/Connection.py

--- DB/Connection.py
+++ DB/Connection.py
@@ -1,17 +1,20 @@
 import sqlite3
-from sqlite3.dbapi2 import connect
 
 #Connect to data base
-connection = sqlite3.connect('PythonStudents.db')
+connection = sqlite3.connect('DB\PythonStudents.db')
 
 #Create a cursor
 Consult = connection.cursor()
-
 #Create table
 Consult.execute("""CREATE TABLE IF NOT EXISTS StudentsList (
         Id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         enrollment INTEGER,
         FirstName VARCHAR(60) NOT NULL,
-        LastName VARCHAR(60) NOT NULL,
-            
+        LastName VARCHAR(60) NOT NULL
     )""")
+
+#Commit our command
+connection.commit()
+
+#Close out connection
+connection.close()
